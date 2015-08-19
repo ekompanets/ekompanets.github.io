0473826ed0ab1d129da3ab0a07586bcfcb91870a
ekompanets <amelie.kom@gmail.com>
2015-08-19 10:20:57 +0300

    12

--- codebars/js/script.js
+++ codebars/js/script.js
@@ -2,20 +2,16 @@
   var link = document.querySelector('.call-link');
   var popup = document.querySelector('.call-popup');
   var close = popup.querySelector('.call-popup-close');
-  var form = popup.querySelector('form');
-  var login = form.querySelector('.login-field');
-  var password = form.querySelector('.password-field');
-  var storage = localStorage.getItem('login');
+  var fade = document.querySelector(".shadow");
 
   link.addEventListener('click', function(event) {
     event.preventDefault();
-    popup.classList.add('call-popup-show');
-    if (storage) {
+    fade.classList.add('call-shadow-show');
   }, false);
 
   close.addEventListener('click', function(event) {
     event.preventDefault();
-    popup.classList.remove('call-popup-show');
+    fade.classList.remove('call-shadow-show');
   }, false);
 
 
